02b9c8ea5434a87a7537bae5a20bda04d7e40002
abindas1 <78995121+abindas1@users.noreply.github.com>
2021-02-15 08:42:58 +0530

    Add files via upload

---
+++ q19.py
@@ -0,0 +1,6 @@
+a=input("enter a string")
+count=0
+for i in a:
+	if (i=="a"or i=="e"or i=="i"or i=="o"or i=="u"or i=="A"or i=="E"or i=="I"or i=="O"or i=="U"):
+		count=count+1
+print("the wovels are:",count)
