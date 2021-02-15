b4ede16b64896c6567db3236c98426004cd574d9
Aadil313 <78995190+Aadil313@users.noreply.github.com>
2021-02-15 09:54:45 +0530

    Add files via upload

---
+++ as2q5.py
@@ -0,0 +1,8 @@
+a = {'one':1,'two':2}
+a.clear()
+print('a =', a)
+dict = {'name':234,'id':15,'age':24}
+element = dict.pop('id')
+print('the popped element is :',element)
+print('the dictionary is:',dict)
+
