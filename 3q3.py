4107759735ec7496e41c4b9104e982a06c69a838
ibinibenny <48010643+ibinibenny@users.noreply.github.com>
2021-02-17 21:51:48 +0530

    Update 3q3.py

--- 3q3.py
+++ 3q3.py
@@ -14,3 +14,11 @@ for i in n:
 	if i not in "a,e,i,o,u,A,E,I,O,U":
 		print(i)
 print(l)
+
+s = input("Enter any string to remove all vowels:")
+vowels = "aeiou"  
+s = s.lower()
+for x in s:
+	if x in vowels:
+		s = s.replace(x, " ")   
+print(list(s))
