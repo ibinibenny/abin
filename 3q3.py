b98c4d182baf2d87739571bafef4b2df214e0792
ibinibenny <48010643+ibinibenny@users.noreply.github.com>
2021-02-17 21:48:38 +0530

    Update 3q3.py

--- 3q3.py
+++ 3q3.py
@@ -14,3 +14,18 @@ for i in n:
 	if i not in "a,e,i,o,u,A,E,I,O,U":
 		print(i)
 print(l)
+
+
+
+
+
+s = input("Enter any string to remove all vowels:")
+vowels = "aeiou"  
+s = s.lower()
+for x in s:
+	if x in vowels:
+		s = s.replace(x, " ")   
+print(list(s))country = ['India', 'Pakistan', 'Nepal', 'Bhutan', 'China', 'Bangladesh']
+capital = ['New Delhi', 'Islamabad','Kathmandu', 'Thimphu', 'Beijing', 'Dhaka']
+print "FOR-loop result: " + str(eg3_for(country, capital))
+print "LC result      : " + str(eg3_lc(country, capital))
